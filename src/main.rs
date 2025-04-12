3c260020e0083afae14450b928763380a15fb0bb
Tom Schroeder <jts13@illinois.edu>
2025-04-12 12:19:38 -0500

    local: WIP

--- src/main.rs
+++ src/main.rs
@@ -1,169 +1,49 @@
 use revm::bytecode::Bytecode;
 use revm::context::ContextTr;
 use revm::database::EmptyDB;
-use revm::handler::instructions::InstructionProvider;
+use revm::handler::EvmTr;
+use revm::handler::instructions::{EthInstructions, InstructionProvider};
+use revm::interpreter::host::DummyHost;
 use revm::interpreter::interpreter::{EthInterpreter, ExtBytecode};
 use revm::interpreter::interpreter_types::{Jumps, LoopControl, MemoryTr};
 use revm::interpreter::{EMPTY_SHARED_MEMORY, InputsImpl, Interpreter};
 use revm::primitives::hardfork::SpecId;
 use revm::primitives::{Address, Bytes, b256};
 use revm::state::{Account, AccountInfo};
-use revm::{Context, MainBuilder};
+use revm::{Context, Journal, MainBuilder};
 use std::cell::RefCell;
 use std::rc::Rc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // // Set up the HTTP transport which is consumed by the RPC client.
-    // let rpc_url = format!("https://mainnet.infura.io/v3/{API_KEY}").parse()?;
-    //
-    // // Create a provider
-    // let client = ProviderBuilder::new().on_http(rpc_url);
-    //
-    // // Params
-    // let chain_id: u64 = 1;
-    // let block_number = 10889447;
-    //
-    // // Fetch the transaction-rich block
-    // let block = match client
-    //     .get_block_by_number(BlockNumberOrTag::Number(block_number))
-    //     .full()
-    //     .await
-    // {
-    //     Ok(Some(block)) => block,
-    //     Ok(None) => anyhow::bail!("Block not found"),
-    //     Err(error) => anyhow::bail!("Error: {:?}", error),
-    // };
-    // println!("Fetched block number: {}", block.header.number);
-    // let previous_block_number = block_number - 1;
-    //
-    // // Use the previous block state as the db with caching
-    // let prev_id: BlockId = previous_block_number.into();
-    // // SAFETY: This cannot fail since this is in the top-level tokio runtime
-    //
-    // let state_db = WrapDatabaseAsync::new(AlloyDB::new(client, prev_id)).unwrap();
-    // let cache_db: CacheDB<_> = CacheDB::new(state_db);
-    // let mut state = StateBuilder::new_with_database(cache_db).build();
-    // let ctx = Context::mainnet()
-    //     .with_db(&mut state)
-    //     .modify_block_chained(|b| {
-    //         b.number = block.header.number;
-    //         b.beneficiary = block.header.beneficiary;
-    //         b.timestamp = block.header.timestamp;
-    //
-    //         b.difficulty = block.header.difficulty;
-    //         b.gas_limit = block.header.gas_limit;
-    //         b.basefee = block.header.base_fee_per_gas.unwrap_or_default();
-    //     })
-    //     .modify_cfg_chained(|c| {
-    //         c.chain_id = chain_id;
-    //     });
-    // std::fs::create_dir_all("target/traces")?;
-    //
-    // let write = OpenOptions::new()
-    //     .write(true)
-    //     .create(true)
-    //     .truncate(true)
-    //     .open("target/traces/0.jsonl");
-    // let inner = Arc::new(Mutex::new(BufWriter::new(
-    //     write.expect("Failed to open file"),
-    // )));
-    // let writer = FlushWriter::new(Arc::clone(&inner));
-    // let mut evm = ctx.build_mainnet_with_inspector(TracerEip3155::new(Box::new(writer)));
-    //
-    // let txs = block.transactions.len().min(5);
-    // println!(
-    //     "Found {} transactions. (Fetching the first {txs}.)",
-    //     block.transactions.len()
-    // );
-    //
-    // let console_bar = Arc::new(ProgressBar::new(txs as u64));
-    // let start = Instant::now();
-    //
-    // // Fill in CfgEnv
-    // let BlockTransactions::Full(transactions) = block.transactions else {
-    //     panic!("Wrong transaction type")
-    // };
-    //
-    // for tx in transactions.iter().take(txs) {
-    //     evm.modify_tx(|etx| {
-    //         etx.caller = tx.inner.signer();
-    //         etx.gas_limit = tx.gas_limit();
-    //         etx.gas_price = tx.gas_price().unwrap_or(tx.inner.max_fee_per_gas());
-    //         etx.value = tx.value();
-    //         etx.data = tx.input().to_owned();
-    //         etx.gas_priority_fee = tx.max_priority_fee_per_gas();
-    //         etx.chain_id = Some(chain_id);
-    //         etx.nonce = tx.nonce();
-    //         if let Some(access_list) = tx.access_list() {
-    //             etx.access_list = access_list.clone()
-    //         } else {
-    //             etx.access_list = Default::default();
-    //         }
-    //
-    //         etx.kind = match tx.to() {
-    //             Some(to_address) => TxKind::Call(to_address),
-    //             None => TxKind::Create,
-    //         };
-    //     });
-    //
-    //     // Construct the file writer to write the trace to
-    //     let tx_number = tx.transaction_index.unwrap_or_default();
-    //     let file_name = format!("target/traces/{}.jsonl", tx_number);
-    //     let write = OpenOptions::new()
-    //         .write(true)
-    //         .create(true)
-    //         .truncate(true)
-    //         .open(file_name)
-    //         .expect("Failed to open file");
-    //     let inner = Arc::new(Mutex::new(BufWriter::new(write)));
-    //     let writer = FlushWriter::new(Arc::clone(&inner));
-    //
-    //     // Inspect and commit the transaction to the EVM
-    //     let res = evm.inspect_replay_with_inspector(TracerEip3155::new(Box::new(writer)));
-    //
-    //     if let Err(error) = res {
-    //         println!("Got error: {:?}", error);
-    //     }
-    //
-    //     // Flush the file writer
-    //     inner.lock().unwrap().flush().expect("Failed to flush file");
-    //
-    //     console_bar.inc(1);
-    // }
-    //
-    // console_bar.finish_with_message("Finished all transactions.");
-    //
-    // let elapsed = start.elapsed();
-    // println!(
-    //     "Finished execution. Total CPU time: {:.6}s",
-    //     elapsed.as_secs_f64()
-    // );
-
     // TODO(toms): can we deconstruct `evm` to simplify things? (and pull the necessary types out of it)
 
     let spec = SpecId::default();
     let ctx: Context = Context::new(EmptyDB::new(), spec);
     let mut evm = ctx.build_mainnet_with_inspector(());
 
+    let instructions = EthInstructions::default();
+    // let mut journal: Journal<EmptyDB> = Journal::new(spec, EmptyDB::default());
+    // let host = &mut DummyHost;
+
+    // let instructions = &mut evm.instruction;
+    let journal = evm.journal();
+    // let host = &mut evm.data.ctx;
+
     let target_address = Address::from_word(b256!(
         "0x00000000000000000000000000000000000000000000000000000000000000F0"
     ));
-    evm.journal()
-        .state()
-        .insert(target_address, Account::default());
+    journal.state().insert(target_address, Account::default());
 
     let caller_address = Address::from_word(b256!(
         "0x00000000000000000000000000000000000000000000000000000000000000FF"
     ));
-    evm.journal()
-        .state()
-        .insert(caller_address, Account::default());
+    journal.state().insert(caller_address, Account::default());
 
     let bytecode_address = Address::from_word(b256!(
         "0x0000000000000000000000000000000000000000000000000000000000000002"
     ));
-    evm.journal().state().insert(
+    journal.state().insert(
         bytecode_address,
         AccountInfo::from_bytecode(Bytecode::new_raw(Bytes::from(&[0x60, 0x00][..]))).into(),
     );
@@ -290,7 +170,8 @@ async fn main() -> anyhow::Result<()> {
     {
         // evm.run_interpreter(&mut interpreter);
 
-        let context = &mut evm.data.ctx;
+        let (host, _instructions) = evm.ctx_instructions();
+
         // let action = interpreter.run_plain(instructions.instruction_table(), context);
         let action = {
             interpreter.reset_control();
@@ -322,8 +203,7 @@ async fn main() -> anyhow::Result<()> {
                 // it will do noop and just stop execution of this contract
                 interpreter.bytecode.relative_jump(1);
 
-                // Execute instruction.
-                evm.instruction.instruction_table()[opcode as usize](&mut interpreter, context)
+                instructions.instruction_table()[opcode as usize](&mut interpreter, host)
             }
 
             interpreter.take_next_action()
@@ -333,3 +213,5 @@ async fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+mod isolate {}
